4beff1ee537472742986a8c8a849c679c8c77e6f
Sandeep Kandula <sandeep.kandula@thermofisher.com>
2021-11-29 19:39:00 +0530

    Implementing Authentication with OpenID Connect.

---
+++ AuthenticationOIDC/src/IdentityServer4.IDP/Config.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace IdentityServer4.IDP
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            {
+                //These are mapped to claims of the user such as first name and last name.
+                //These are subclaim and also known as user identifier.
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile() //Given Name and Family Name
+
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            { };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            { };
+    }
+}
