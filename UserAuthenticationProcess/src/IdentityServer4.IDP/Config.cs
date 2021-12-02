f6ef8317f6e716dd5ad48466da73f2454fc7559a
Sandeep Kandula <sandeep.kandula@thermofisher.com>
2021-12-02 18:30:07 +0530

    Securing User Authentication Process

---
+++ UserAuthenticationProcess/src/IdentityServer4.IDP/Config.cs
@@ -0,0 +1,66 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace IdentityServer4.IDP
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            {
+                //These are mapped to claims of the user such as first name and last name.
+                //These are subclaim and also known as user identifier.
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile() //Given Name and Family Name
+
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            { };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                //Configuring Identity Server to login with Authorization Code Flow
+                new Client
+                {
+                    ClientName = "Image Gallery",
+                    ClientId = "imagegalleryclient",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequireConsent = true,
+                    RequirePkce = true,
+                    RedirectUris = new List<string>
+                    {
+                        //sigin-oidc is the default value. This can be configured at web client.
+                        //URL of the imagegalleryclient
+                        "https://localhost:44389/signin-oidc"
+                    },
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile
+                    },
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    #region Redirect URI after logging out
+                    PostLogoutRedirectUris = new List<string>
+                    {
+                        //sigin-oidc is the default value. This can be configured at web client.
+                        //URL of the imagegalleryclient
+                        "https://localhost:44389/signout-callback-oidc"
+
+                    }
+                    #endregion
+                    
+                }
+
+            };
+    }
+}
---
+++ UserAuthenticationProcess/src/IdentityServer4.IDP/Quickstart/TestUsers.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityModel;
+using IdentityServer4.Test;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text.Json;
+using IdentityServer4;
+
+namespace IdentityServerHost.Quickstart.UI
+{
+    public class TestUsers
+    {
+        public static List<TestUser> Users
+        {
+            get
+            {                                
+                return new List<TestUser>
+                {
+                    new TestUser
+                    {
+                        SubjectId = "d860efca-22d9-47fd-8249-791ba61b07c7",
+                        Username = "Frank",
+                        Password = "password",
+                        Claims =
+                        {
+                            
+                            new Claim(JwtClaimTypes.GivenName, "Frank"),
+                            new Claim(JwtClaimTypes.FamilyName, "Underwood"),
+                            
+                        }
+                    },
+                    new TestUser
+                    {
+                        SubjectId = "b7539694-97e7-4dfe-84da-b4256e1ff5c7",
+                        Username = "Claire",
+                        Password = "password",
+                        Claims =
+                        {
+                            
+                            new Claim(JwtClaimTypes.GivenName, "Claire"),
+                            new Claim(JwtClaimTypes.FamilyName, "Underwood"),
+                           
+                        }
+                    }
+                };
+            }
+        }
+    }
+}
