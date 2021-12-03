c682b15ef27b1ce295261a4b5c3c8148de9945ba
Sandeep Kandula <sandeep.kandula@thermofisher.com>
2021-12-03 18:27:48 +0530

    Claims in the web application

---
+++ Claims/src/IdentityServer4.IDP/Config.cs
@@ -0,0 +1,70 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace IdentityServer4.IDP
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            {
+                //These are mapped to claims of the user such as first name and last name.
+                //These are subclaim and also known as user identifier.
+                new IdentityResources.OpenId(),
+                //Given Name and Family Name
+                new IdentityResources.Profile(),
+                //Getting ready for calling the UserInfo endpoint
+                new IdentityResources.Address()
+
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            { };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                //Configuring Identity Server to login with Authorization Code Flow
+                new Client
+                {
+                    ClientName = "Image Gallery",
+                    ClientId = "imagegalleryclient",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequireConsent = true,
+                    RequirePkce = true,
+                    RedirectUris = new List<string>
+                    {
+                        //sigin-oidc is the default value. This can be configured at web client.
+                        //URL of the imagegalleryclient
+                        "https://localhost:44389/signin-oidc"
+                    },
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.Address                       
+                    },
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    #region Redirect URI after logging out
+                    PostLogoutRedirectUris = new List<string>
+                    {
+                        //sigin-oidc is the default value. This can be configured at web client.
+                        //URL of the imagegalleryclient
+                        "https://localhost:44389/signout-callback-oidc"
+
+                    }
+                    #endregion
+                    
+                }
+
+            };
+    }
+}
