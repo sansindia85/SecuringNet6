611e2407f667d883ac96e7a44d82661f2b1c1d30
Sandeep Kandula <sandeep.kandula@thermofisher.com>
2021-12-12 18:51:29 +0530

    Token expiration and reference token.

---
+++ TokenLifetime/src/IdentityServer4.IDP/Quickstart/TestUsers.cs
@@ -0,0 +1,58 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityModel;
+using IdentityServer4.Test;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text.Json;
+using IdentityServer4;
+
+namespace IdentityServerHost.Quickstart.UI
+{
+    public class TestUsers
+    {
+        public static List<TestUser> Users
+        {
+            get
+            {                                
+                return new List<TestUser>
+                {
+                    new TestUser
+                    {
+                        SubjectId = "d860efca-22d9-47fd-8249-791ba61b07c7",
+                        Username = "Frank",
+                        Password = "password",
+                        Claims =
+                        {
+                            
+                            new Claim(JwtClaimTypes.GivenName, "Frank"),
+                            new Claim(JwtClaimTypes.FamilyName, "Underwood"),
+                            new Claim(JwtClaimTypes.Address, "Main Road 1"),
+                            new Claim(JwtClaimTypes.Role, "FreeUser"),
+                            new Claim("subscriptionlevel", "FreeUser"),
+                            new Claim("country", "nl")
+                        }
+                    },
+                    new TestUser
+                    {
+                        SubjectId = "b7539694-97e7-4dfe-84da-b4256e1ff5c7",
+                        Username = "Claire",
+                        Password = "password",
+                        Claims =
+                        {
+                            
+                            new Claim(JwtClaimTypes.GivenName, "Claire"),
+                            new Claim(JwtClaimTypes.FamilyName, "Underwood"),
+                            new Claim(JwtClaimTypes.Address, "Big Street 2"),
+                            new Claim(JwtClaimTypes.Role, "PayingUser"),
+                            new Claim("subscriptionlevel", "PayingUser"),
+                            new Claim("country", "be")
+                        }
+                    }
+                };
+            }
+        }
+    }
+}
